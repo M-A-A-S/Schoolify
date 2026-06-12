e63a0b44f7a6190c3d3ca7070ff91d134466f972
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-06-12 09:10:01 +0200

    Add Student Repo

---
+++ Schoolify.Common/DTOs/Student/StudentDTO.cs
@@ -0,0 +1,21 @@
+﻿using Schoolify.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoolify.Common.DTOs.Student
+{
+    public class StudentDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string ThirdName { get; set; }
+        public string ForthName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public DateOnly EnrolmentDate { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
---
+++ Schoolify.Common/Extensions/StudentExtensions.cs
@@ -0,0 +1,70 @@
+﻿using Schoolify.Common.DTOs.Student;
+using Schoolify.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoolify.Common.Extensions
+{
+    public static class StudentExtensions
+    {
+        public static StudentDTO ToDTO(this Student entity)
+        {
+            if (entity == null)
+            {
+                return null;
+            }
+
+            return new StudentDTO
+            {
+                Id = entity.Id,
+                FirstName = entity.FirstName,
+                SecondName = entity.SecondName,
+                ThirdName = entity.ThirdName,
+                ForthName = entity.ForthName,
+                DateOfBirth = entity.DateOfBirth,
+                EnrolmentDate = entity.EnrolmentDate,
+                Gender = entity.Gender,
+            };
+        }
+
+        public static Student ToEntity(this StudentDTO DTO)
+        {
+            if (DTO == null)
+            {
+                return null;
+            }
+
+            return new Student
+            {
+                Id = DTO.Id,
+                FirstName = DTO.FirstName,
+                SecondName = DTO.SecondName,
+                ThirdName = DTO.ThirdName,
+                ForthName = DTO.ForthName,
+                DateOfBirth = DTO.DateOfBirth,
+                EnrolmentDate = DTO.EnrolmentDate,
+                Gender = DTO.Gender,
+            };
+        }
+
+        public static void UpdateFromDTO(this Student entity, StudentDTO DTO)
+        {
+
+            if (entity == null || DTO == null)
+            {
+                return;
+            }
+
+            entity.FirstName = DTO.FirstName;
+            entity.SecondName = DTO.SecondName;
+            entity.ThirdName = DTO.ThirdName;
+            entity.ForthName = DTO.ForthName;
+            entity.DateOfBirth = DTO.DateOfBirth;
+            entity.EnrolmentDate = DTO.EnrolmentDate;
+            entity.Gender = DTO.Gender;
+        }
+    }
+}
