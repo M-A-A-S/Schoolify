55c6b0c0ce6c4f4a9b4a7df6d978738adf167926
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-06-12 07:15:11 +0200

    Add Guardian Repository

---
+++ Schoolify.Common/DTOs/Guardian/GuardianDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoolify.Common.DTOs.Guardian
+{
+    public class GuardianDTO
+    {
+        public int Id { get; set; } 
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string ThirdName { get; set; }
+        public string ForthName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Schoolify.DataAccess/Repositories/GuardianRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using Schoolify.Common.Models;
+using Schoolify.DataAccess.Data;
+using Schoolify.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoolify.DataAccess.Repositories
+{
+    public class GuardianRepository : Repository<Guardian>, IGuardianRepository
+    {
+        public GuardianRepository(AppDbContext context, ILogger<Guardian> logger) : base(context, logger)
+        {
+
+        }
+    }
+}
