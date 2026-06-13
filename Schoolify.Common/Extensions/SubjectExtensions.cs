b0a0636bac66d3657b338ac0920a14b866c7859b
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-06-13 08:52:22 +0200

    Add Subject Repository

---
+++ Schoolify.Common/Extensions/SubjectExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Schoolify.Common.DTOs.Subject;
+using Schoolify.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoolify.Common.Extensions
+{
+    public static class SubjectExtensions
+    {
+        public static SubjectDTO ToDTO(this Subject entity)
+        {
+            if (entity == null)
+            {
+                return null;
+            }
+
+            return new SubjectDTO
+            {
+                Id = entity.Id,
+                DepartmentId = entity.DepartmentId,
+                NameEn = entity.NameEn,
+                NameAr = entity.NameAr
+            };
+        }
+
+        public static Subject ToEntity(this SubjectDTO DTO)
+        {
+            if (DTO == null)
+            {
+                return null;
+            }
+
+            return new Subject
+            {
+                Id = DTO.Id,
+                DepartmentId = DTO.DepartmentId,
+                NameEn = DTO.NameEn,
+                NameAr = DTO.NameAr
+            };
+        }
+
+        public static void UpdateFromDTO(this Subject entity, SubjectDTO DTO)
+        {
+
+            if (entity == null || DTO == null)
+            {
+                return;
+            }
+
+            entity.DepartmentId = DTO.DepartmentId;
+            entity.NameEn = DTO.NameEn;
+            entity.NameAr = DTO.NameAr;
+        }
+    
+    }
+}
---
+++ Schoolify.DataAccess/Repositories/SubjectRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using Schoolify.Common.Models;
+using Schoolify.DataAccess.Data;
+using Schoolify.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoolify.DataAccess.Repositories
+{
+    public class SubjectRepository : Repository<Student>, ISubjectRepository
+    {
+        public SubjectRepository(AppDbContext context, ILogger<Student> logger) : base(context, logger)
+        {
+
+        }
+    }
+}
