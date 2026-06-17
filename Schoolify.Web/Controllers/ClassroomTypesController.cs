dfd7587d210fe820e797cae721d9926c446ce39f
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-06-17 10:51:33 +0200

    Add ClassroomTypes Controller

---
+++ Schoolify.Web/Controllers/ClassroomTypesController.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Localization;
+using Schoolify.Business.Interfaces;
+using Schoolify.Common;
+using Schoolify.Common.DTOs.ClassroomType;
+using System.Globalization;
+
+namespace Schoolify.Web.Controllers
+{
+    public class ClassroomTypesController : Controller
+    {
+        private readonly IClassroomTypeService _service;
+        private readonly IStringLocalizer<SharedResource> _localizer;
+
+        public ClassroomTypesController(IClassroomTypeService service,
+            IStringLocalizer<SharedResource> localizer)
+        {
+            _service = service;
+            _localizer = localizer;
+        }
+
+        #region Get
+        public async Task<IActionResult> Index()
+        {
+            var findAllClassroomTypesResult = await _service.GetAllAsync();
+            return View(findAllClassroomTypesResult.Data);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var findResult = await _service.GetByIdAsync(id);
+            if (findResult.Data == null || !findResult.IsSuccess)
+            {
+                TempData["Error"] = _localizer[findResult.Code].Value;
+                return NotFound();
+            }
+            return View(findResult.Data);
+        }
+        #endregion
+
+        #region Create
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ClassroomTypeDTO DTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["Error"] = _localizer["ValidationError"].Value;
+                return View(DTO);
+            }
+
+            var addResult = await _service.AddAsync(DTO);
+
+            if (addResult.IsSuccess)
+            {
+                TempData["Success"] = _localizer[addResult.Code].Value;
+                return RedirectToAction(nameof(Index));
+            }
+
+            TempData["Error"] = _localizer[addResult.Code].Value;
+            return View(DTO);
+        }
+        #endregion
+
+        #region Update
+        public async Task<IActionResult> Edit(int id)
+        {
+            var findResult = await _service.GetByIdAsync(id);
+            if (findResult.Data == null || !findResult.IsSuccess)
+            {
+                TempData["Error"] = _localizer[findResult.Code].Value;
+                return NotFound();
+            }
+
+            return View(findResult.Data);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ClassroomTypeDTO DTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["Error"] = _localizer["ValidationError"].Value;
+                return View(DTO);
+            }
+
+            var updateResult = await _service.UpdateAsync(DTO.Id, DTO);
+            if (updateResult.IsSuccess)
+            {
+                TempData["Success"] = _localizer[updateResult.Code].Value;
+                return RedirectToAction(nameof(Index));
+            }
+
+            TempData["Error"] = _localizer[updateResult.Code].Value;
+            return View(DTO);
+        }
+        #endregion
+
+        #region Delete
+        public async Task<IActionResult> Delete(int id)
+        {
+            var findResult = await _service.GetByIdAsync(id);
+            if (findResult.Data == null || !findResult.IsSuccess)
+            {
+                TempData["Error"] = _localizer[findResult.Code].Value;
+                return NotFound();
+            }
+            return View(findResult.Data);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+
+            var deleteResult = await _service.DeleteAsync(id);
+
+            if (deleteResult.IsSuccess)
+            {
+                TempData["Success"] = _localizer[deleteResult.Code].Value;
+                return RedirectToAction(nameof(Index));
+            }
+
+            TempData["Error"] = _localizer[deleteResult.Code].Value;
+            return View(deleteResult.Data);
+        }
+        #endregion
+
+
+    }
+}
