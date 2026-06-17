281809a142aacec0807f738598d4b29db04b38b7
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-06-17 11:05:17 +0200

    Add ClassroomType create and index views

--- Schoolify.Web/Program.cs
+++ Schoolify.Web/Program.cs
@@ -94,11 +94,13 @@ localizationOptions.RequestCultureProviders = new IRequestCultureProvider[]
 
 app.UseRequestLocalization(localizationOptions);
 
+app.UseExceptionHandler("/Error/500");
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     //app.UseExceptionHandler("/Home/Error");
-    app.UseExceptionHandler("/Error/500");
+    //app.UseExceptionHandler("/Error/500");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
