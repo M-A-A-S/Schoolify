657d66825e2ea566f45fb6c9732b6653a6aa4cde
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-06-19 10:32:12 +0200

    Add Section Create view

--- Schoolify.Common/DTOs/Section/SectionDTO.cs
+++ Schoolify.Common/DTOs/Section/SectionDTO.cs
@@ -1,6 +1,7 @@
 ﻿using Schoolify.Common.DTOs.YearLevel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,33 @@ namespace Schoolify.Common.DTOs.Section
     public class SectionDTO
     {  
         public int Id { get; set; }
+
+        [Display(Name = nameof(Resources.SharedResource.YearLevel),
+ResourceType = typeof(Resources.SharedResource)
+)]
+        [Required(
+ErrorMessageResourceName = nameof(Resources.SharedResource.Required),
+ErrorMessageResourceType = typeof(Resources.SharedResource)
+)]
         public int YearLevelId { get; set; }
         //public int? ClassroomId { get; set; }
+
+        [Display(Name = nameof(Resources.SharedResource.NameEn),
+ResourceType = typeof(Resources.SharedResource)
+)]
+        [Required(
+ErrorMessageResourceName = nameof(Resources.SharedResource.Required),
+ErrorMessageResourceType = typeof(Resources.SharedResource)
+)]
         public string NameEn { get; set; } // A
+
+        [Display(Name = nameof(Resources.SharedResource.NameAr),
+ResourceType = typeof(Resources.SharedResource)
+)]
+        [Required(
+ErrorMessageResourceName = nameof(Resources.SharedResource.Required),
+ErrorMessageResourceType = typeof(Resources.SharedResource)
+)]
         public string NameAr { get; set; } // أ
         public YearLevelDTO? YearLevel { get; set; }
     }
