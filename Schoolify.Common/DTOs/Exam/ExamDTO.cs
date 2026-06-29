74ea89a0e3f66eb788e62018c50ac234feb7df84
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-06-29 09:12:58 +0200

    Add Exam Create view

--- Schoolify.Common/DTOs/Exam/ExamDTO.cs
+++ Schoolify.Common/DTOs/Exam/ExamDTO.cs
@@ -3,6 +3,7 @@ using Schoolify.Common.DTOs.StudentExamResult;
 using Schoolify.Common.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,50 @@ namespace Schoolify.Common.DTOs.Exam
     public class ExamDTO
     {
         public int Id { get; set; }
+
+        [Display(Name = nameof(Resources.SharedResource.Class),
+ResourceType = typeof(Resources.SharedResource)
+)]
+        [Required(
+ErrorMessageResourceName = nameof(Resources.SharedResource.Required),
+ErrorMessageResourceType = typeof(Resources.SharedResource)
+)]
         public int SubjectClassId { get; set; }
+
+        [Display(Name = nameof(Resources.SharedResource.NameEn),
+ResourceType = typeof(Resources.SharedResource)
+)]
+        [Required(
+ErrorMessageResourceName = nameof(Resources.SharedResource.Required),
+ErrorMessageResourceType = typeof(Resources.SharedResource)
+)]
         public string NameEn { get; set; } // Midterm / Final
+
+        [Display(Name = nameof(Resources.SharedResource.NameAr),
+ResourceType = typeof(Resources.SharedResource)
+)]
+        [Required(
+ErrorMessageResourceName = nameof(Resources.SharedResource.Required),
+ErrorMessageResourceType = typeof(Resources.SharedResource)
+)]
         public string NameAr { get; set; } // 
+
+        [Display(Name = nameof(Resources.SharedResource.MaxScore),
+ResourceType = typeof(Resources.SharedResource)
+)]
+        [Required(
+ErrorMessageResourceName = nameof(Resources.SharedResource.Required),
+ErrorMessageResourceType = typeof(Resources.SharedResource)
+)]
         public decimal MaxScore { get; set; }
+
+        [Display(Name = nameof(Resources.SharedResource.Date),
+ResourceType = typeof(Resources.SharedResource)
+)]
+        [Required(
+ErrorMessageResourceName = nameof(Resources.SharedResource.Required),
+ErrorMessageResourceType = typeof(Resources.SharedResource)
+)]
         public DateTime Date { get; set; }
         public SubjectClassDTO? SubjectClass { get; set; }
         public ICollection<StudentExamResultDTO> StudentExamResults { get; set; } = new List<StudentExamResultDTO>();
