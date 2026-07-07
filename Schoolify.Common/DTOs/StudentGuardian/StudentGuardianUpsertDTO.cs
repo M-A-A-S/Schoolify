f72ca5ace289ac81c2caafb57da1c3882a95c8b6
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-07-07 11:41:08 +0200

    Add StudentGuardians Controller

---
+++ Schoolify.Common/DTOs/StudentGuardian/StudentGuardianUpsertDTO.cs
@@ -0,0 +1,25 @@
+﻿using Schoolify.Common.DTOs.Class;
+using Schoolify.Common.DTOs.Guardian;
+using Schoolify.Common.DTOs.GuardianType;
+using Schoolify.Common.DTOs.Section;
+using Schoolify.Common.DTOs.Student;
+using Schoolify.Common.DTOs.Subject;
+using Schoolify.Common.DTOs.Teacher;
+using Schoolify.Common.DTOs.Term;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoolify.Common.DTOs.StudentGuardian
+{
+    public class StudentGuardianUpsertDTO
+    {
+        public StudentGuardianDTO StudentGuardian { get; set; } = new();
+
+        public List<StudentDTO> Students { get; set; } = new();
+        public List<GuardianDTO> Guardians { get; set; } = new();
+        public List<GuardianTypeDTO> GuardianTypes { get; set; } = new();
+    }
+}
---
+++ Schoolify.Web/Controllers/StudentGuardiansController.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Localization;
+using Schoolify.Business.Interfaces;
+using Schoolify.Common;
+using Schoolify.Common.DTOs.StudentGuardian;
+using System.Globalization;
+
+namespace Schoolify.Web.Controllers
+{
+    public class StudentGuardiansController : Controller
+    {
+        private readonly IStudentGuardianService _service;
+        private readonly IStudentService _studentService;
+        private readonly IGuardianService _guardianService;
+        private readonly IGuardianTypeService _guardianTypeService;
+        private readonly IStringLocalizer<SharedResource> _localizer;
+
+        public StudentGuardiansController(IStudentGuardianService service,
+            IStringLocalizer<SharedResource> localizer,
+            IStudentService studentService,
+            IGuardianService guardianService,
+            IGuardianTypeService guardianTypeService)
+        {
+            _service = service;
+            _localizer = localizer;
+            _studentService = studentService;
+            _guardianService = guardianService;
+            _guardianTypeService = guardianTypeService;
+        }
+
+        #region Get
+        public async Task<IActionResult> Index()
+        {
+            var findAllStudentGuardiansResult = await _service.GetAllAsync();
+            return View(findAllStudentGuardiansResult.Data);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var findResult = await _service.GetByIdAsync(id);
+            if (findResult.Data == null || !findResult.IsSuccess)
+            {
+                TempData["Error"] = _localizer[findResult.Code].Value;
+                return NotFound();
+            }
+            return View(findResult.Data);
+        }
+        #endregion
+
+        #region Create
+        public async Task<IActionResult> Create()
+        {
+            return View(await BuildStudentGuardianUpsertDTO(new StudentGuardianDTO()));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(StudentGuardianUpsertDTO DTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["Error"] = _localizer["ValidationError"].Value;
+                return View(await BuildStudentGuardianUpsertDTO(DTO.StudentGuardian));
+            }
+
+            var addResult = await _service.AddAsync(DTO.StudentGuardian);
+
+            if (addResult.IsSuccess)
+            {
+                TempData["Success"] = _localizer[addResult.Code].Value;
+                //return RedirectToAction(nameof(Index));
+                //return RedirectToAction("Index", "StudentGuardians", new { Id = addResult?.Data?.Id });
+            }
+
+            TempData["Error"] = _localizer[addResult.Code].Value;
+            return View(DTO);
+        }
+        #endregion
+
+        #region Update
+        public async Task<IActionResult> Edit(int id)
+        {
+            var findResult = await _service.GetByIdAsync(id);
+            if (findResult.Data == null || !findResult.IsSuccess)
+            {
+                TempData["Error"] = _localizer[findResult.Code].Value;
+                return NotFound();
+            }
+            return View(await BuildStudentGuardianUpsertDTO(findResult.Data));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(StudentGuardianUpsertDTO DTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["Error"] = _localizer["ValidationError"].Value;
+                return View(await BuildStudentGuardianUpsertDTO(DTO.StudentGuardian));
+            }
+
+            var updateResult = await _service.UpdateAsync(DTO.StudentGuardian.Id, DTO.StudentGuardian);
+            if (updateResult.IsSuccess)
+            {
+                TempData["Success"] = _localizer[updateResult.Code].Value;
+                //return RedirectToAction(nameof(Index));
+                return RedirectToAction("Details", "StudentGuardians", new { Id = DTO?.StudentGuardian?.Id });
+            }
+
+            TempData["Error"] = _localizer[updateResult.Code].Value;
+            return View(await BuildStudentGuardianUpsertDTO(DTO.StudentGuardian));
+        }
+        #endregion
+
+        #region Delete
+        public async Task<IActionResult> Delete(int id)
+        {
+            var findResult = await _service.GetByIdAsync(id);
+            if (findResult.Data == null || !findResult.IsSuccess)
+            {
+                TempData["Error"] = _localizer[findResult.Code].Value;
+                return NotFound();
+            }
+            return View(findResult.Data);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+
+            var deleteResult = await _service.DeleteAsync(id);
+
+            if (deleteResult.IsSuccess)
+            {
+                TempData["Success"] = _localizer[deleteResult.Code].Value;
+                return RedirectToAction(nameof(Index));
+            }
+
+            TempData["Error"] = _localizer[deleteResult.Code].Value;
+            return View(deleteResult.Data);
+        }
+        #endregion
+
+        #region Private Helpers
+        private async Task<StudentGuardianUpsertDTO> BuildStudentGuardianUpsertDTO(StudentGuardianDTO? studentGuardian)
+        {
+            //var studentsResult = await _studentService.GetAllAsync();
+            //var guardiansResult = await _guardianService.GetAllAsync();
+            //var guardianTypesResult = await _guardianTypeService.GetAllAsync();
+
+            var studentsResult = _studentService.GetAllAsync();
+            var guardiansResult = _guardianService.GetAllAsync();
+            var guardianTypesResult = _guardianTypeService.GetAllAsync();
+
+            await Task.WhenAll(studentsResult, guardiansResult, guardianTypesResult);
+
+
+            return new StudentGuardianUpsertDTO
+            {
+                StudentGuardian = studentGuardian ?? new(),
+
+                Students = studentsResult?.Result?.Data?.ToList() ?? new(),
+                Guardians = guardiansResult?.Result?.Data?.ToList() ?? new(),
+                GuardianTypes = guardianTypesResult?.Result?.Data?.ToList() ?? new()
+            };
+        }
+
+        #endregion
+
+    }
+}
